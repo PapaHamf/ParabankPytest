3450a7ec01946e73d613e7152c58c7a61f8a5f7e
PapaHamf <jaroslaw.puchala@gmail.com>
2025-01-23 18:23:54 +0100

    ExcelData class update 23.01

--- testdata/exceldata.py
+++ testdata/exceldata.py
@@ -1,8 +1,16 @@
+import datetime
+import os
+
 import openpyxl
+from datetime import datetime
 from openpyxl.workbook import Workbook
 
 
 class ExcelData():
+
+    def __init__(self, testcase: str):
+        self._data: dict = {}
+        self._testcase: str = testcase
 
     @staticmethod
     def get_excel_data(file_name: str, sheet_name: str = None) -> list [dict]:
@@ -45,6 +53,7 @@ class ExcelData():
         sheet.title = "Data"
         # Adding the header w/ column names
         column_names = data[0].keys()
+        # Start from the 1 because the cells are indexed from 1
         for key, name in enumerate(column_names, 1):
             sheet.column_dimensions[sheet.cell(row = 1, column = key).column_letter].width = 30
             sheet.cell(row = 1, column = key).value = name
@@ -54,5 +63,90 @@ class ExcelData():
             for c_key, column in enumerate(row, 1):
                 sheet.cell(row = r_key + 1, column = c_key).value = values[c_key - 1]
         # Saving the file
-        # book.save(f"../testdata/{file_name}")
-        book.save(f"./{file_name}")
+        book.save(f"../testdata/{file_name}")
+        # book.save(f"./{file_name}")
+
+    def add_data(self, key: str, value: str) -> None:
+        """
+        Allows you to add the test case data to the dictionary.
+        :param key: The key of the dictionary entry.
+        :param value: The value of the dictionary entry.
+        :return:
+        """
+        self._data[key] = value
+
+    def remove_data_by_key(self, key: str) -> None:
+        """
+        Allows you to remove the test case data from the dictionary by key.
+        :param key: The key of the dictionary entry.
+        :return:
+        """
+        self._data.pop(key)
+
+    def remove_data_by_value(self, value: str) -> None:
+        """
+        Allows you to remove the test case data from the dictionary by value.
+        :param value: The value of the dictionary entry.
+        :return:
+        """
+        self._data = { key:val for key, val in self._data.items() if val != value }
+
+    def save_data(self) -> None:
+        """
+        Saves the data to the Excel file.
+        The file name is created based on the date & the test case name.
+        :return:
+        """
+        book = Workbook()
+        sheet = book.active
+        sheet.title = f"Data: {self._testcase}"
+        # Adding the header w/ column names
+        column_names = self._data.keys()
+        # Start from the number 2 to make room for the test case name
+        for key, name in enumerate(column_names, 2):
+            sheet.column_dimensions[sheet.cell(row = 1, column = key).column_letter].width = 30
+            sheet.cell(row = 1, column = key).value = name
+        # Adding the test case name
+        sheet.cell(row = 2, column = 1).value = self._testcase
+        # Adding the data in columns
+        values: list = list(self._data.values())
+        for c_key, column in enumerate(self._data, 2):
+            sheet.cell(row = 2, column = c_key).value = values[c_key - 1]
+        # Saving the file
+        date = datetime.now().date()
+        time = datetime.now().time().strftime("%H:%M:%S")
+        file_name = f"{self._testcase}_{date}_{time}.xlsx"
+        book.save(f"../testdata/{file_name}")
+
+
+    def read_data(self, testcase: str, date: str) -> dict | list [dict]:
+        """
+        Reads the data from the Excel file or mutiple files if there are multiple test cases
+        with the same name & date.
+        :param testcase: The name of the testcase for which the data should be read.
+        :param date: The date when the testcase was ran.
+        :return:
+        """
+        files = os.listdir("../")
+        valid_files = [file for file in files if testcase in file and date in file]
+        if len(valid_files) == 1:
+            book = openpyxl.load_workbook(f"../testdata/{valid_files[0]}")
+            # Reading the data
+            for row in range(2, sheet.max_row + 1):
+                excel_data: dict = {}
+                for column in range(1, sheet.max_column + 1):
+                    excel_data[sheet.cell(row=1, column=column).value] = sheet.cell(row=row, column=column).value
+        elif len(valid_files) > 1:
+            for file in valid_files:
+                book = openpyxl.load_workbook(f"../testdata/{file}")
+                # Declaring temporary list
+                excel_data: list = []
+                # Reading the data
+                for row in range(2, sheet.max_row + 1):
+                    temp_dict: dict = {}
+                    for column in range(1, sheet.max_column + 1):
+                        temp_dict[sheet.cell(row=1, column=column).value] = sheet.cell(row=row, column=column).value
+                    excel_data.append(temp_dict)
+        else:
+            excel_data = None
+        return excel_data
