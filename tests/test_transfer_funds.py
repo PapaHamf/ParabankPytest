42cd69e5cffa22342802437fd637b02ad1773072
PapaHamf <jaroslaw.puchala@gmail.com>
2025-03-18 20:47:50 +0100

    Transfer funds TCs 18.03

--- tests/test_transfer_funds.py
+++ tests/test_transfer_funds.py
@@ -50,6 +50,7 @@ class TestOpenAccount(BaseClass):
     @allure.description("This test attempts to transfer funds from one account to other.")
     @pytest.mark.smoke
     @pytest.mark.usefixtures("login_logout")
+    @pytest.mark.skip
     def test_transfer_funds_postive(self, login_logout):
         """
         Tests the funds transfer between accounts.
@@ -87,6 +88,7 @@ class TestOpenAccount(BaseClass):
     @allure.description("This test verifies if the selected source account is removed from the target account list.")
     @pytest.mark.smoke
     @pytest.mark.usefixtures("login_logout")
+    @pytest.mark.skip
     def test_transfer_funds_removed_from_target(self, login_logout):
         """
         Tests if the source account is not on target account list.
@@ -120,6 +122,7 @@ class TestOpenAccount(BaseClass):
     @allure.description("This test attempts to transfer funds from the account with insufficient funds.")
     @pytest.mark.smoke
     @pytest.mark.usefixtures("login_logout")
+    @pytest.mark.skip
     def test_transfer_funds_insufficient_funds(self, login_logout):
         """
         Tests the funds transfer with insufficient funds.
@@ -168,6 +171,7 @@ class TestOpenAccount(BaseClass):
     @allure.description("This test attempts to transfer funds between the same account.")
     @pytest.mark.smoke
     @pytest.mark.usefixtures("login_logout")
+    @pytest.mark.skip
     def test_transfer_funds_same_account(self, login_logout):
         """
         Tests the funds transfer between the same account.
@@ -193,4 +197,113 @@ class TestOpenAccount(BaseClass):
         log.info("Clicking the Transfer button.")
         transfer_funds.get_transfer_button().click()
         assert transfer_funds.get_internal_error().text == transfer_funds.TRANSFER_ERROR_MSG
+
+    @allure.parent_suite("Tests for Parabank application")
+    @allure.suite("Tests for funds transfer")
+    @allure.sub_suite("Positive funds transfer tests")
+    @allure.tag("Funds transfer", "New transfer", "Verify accounts")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.label("owner", "Parasoft")
+    @allure.testcase("Test case no 84")
+    @allure.description("This test attempts to transfer funds from one account to other and verifies"
+                        " if the source and target accounts are correct.")
+    @pytest.mark.smoke
+    @pytest.mark.usefixtures("login_logout")
+    @pytest.mark.skip
+    def test_transfer_funds_verify_accounts(self, login_logout):
+        """
+        Tests the funds transfer and verifies if the accounts are correct.
+        :return:
+        """
+        log = self.get_logger()
+        log.info("Clicking the Transfer Funds link.")
+        transfer_funds = login_logout[0].get_transfer_funds_page()
+        data_collection = ExcelData("Funds transfer", "Verify accounts")
+        amount = str(random.randint(0, 10000))
+        data_collection.add_data("amount", amount)
+        log.info(f"Entering the transfer amount {amount}.")
+        transfer_funds.get_amount().send_keys(amount)
+        source_accounts_list = transfer_funds.get_list_values(transfer_funds.get_source_accounts())
+        src_acc_no = random.choice(source_accounts_list)
+        data_collection.add_data("source_account", src_acc_no)
+        log.info(f"Selecting the source account {src_acc_no}")
+        transfer_funds.select_value_from_dropdown_text(transfer_funds.get_source_accounts(), src_acc_no)
+        target_accounts_list = transfer_funds.get_list_values(transfer_funds.get_target_accounts())
+        trg_acc_no = random.choice(target_accounts_list)
+        data_collection.add_data("target_account", trg_acc_no)
+        log.info(f"Selecting the target account {trg_acc_no}")
+        transfer_funds.select_value_from_dropdown_text(transfer_funds.get_target_accounts(), trg_acc_no)
+        log.info("Clicking the Transfer button.")
+        transfer_funds.get_transfer_button().click()
+        with allure.step("Step 1: Verify if the transfer was successful"):
+            assert transfer_funds.get_transfer_complete().text == transfer_funds.TRANSFER_SUCCESS_MSG
+        with (allure.step("Step 2: Verify the account numbers")):
+            assert transfer_funds.get_source_account_result().text == src_acc_no \
+                    and transfer_funds.get_target_account_result().text == trg_acc_no
+
+    @allure.parent_suite("Tests for Parabank application")
+    @allure.suite("Tests for funds transfer")
+    @allure.sub_suite("Positive funds transfer tests")
+    @allure.tag("Funds transfer", "New transfer", "Verify transfer")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @allure.label("owner", "Parasoft")
+    @allure.testcase("Test case no 85")
+    @allure.description("This test attempts to transfer funds from one account to other and verifies"
+                        " if the funds were transferred correctly.")
+    @pytest.mark.smoke
+    @pytest.mark.usefixtures("login_logout")
+    @pytest.mark.skip
+    def test_transfer_funds_verify_transfer(self, login_logout):
+        """
+        Tests the funds transfer and verifies if the funds were transferred correctly.
+        :return:
+        """
+        log = self.get_logger()
+        log.info("Clicking the Transfer Funds link.")
+        transfer_funds = login_logout[0].get_transfer_funds_page()
+        data_collection = ExcelData("Funds transfer", "Verify transfer")
+        amount = str(random.randint(0, 10000))
+        data_collection.add_data("amount", amount)
+        log.info(f"Entering the transfer amount {amount}.")
+        transfer_funds.get_amount().send_keys(amount)
+        source_accounts_list = transfer_funds.get_list_values(transfer_funds.get_source_accounts())
+        src_acc_no = random.choice(source_accounts_list)
+        data_collection.add_data("source_account", src_acc_no)
+        log.info(f"Selecting the source account {src_acc_no}")
+        transfer_funds.select_value_from_dropdown_text(transfer_funds.get_source_accounts(), src_acc_no)
+        target_accounts_list = transfer_funds.get_list_values(transfer_funds.get_target_accounts())
+        trg_acc_no = random.choice(target_accounts_list)
+        data_collection.add_data("target_account", trg_acc_no)
+        log.info(f"Selecting the target account {trg_acc_no}")
+        transfer_funds.select_value_from_dropdown_text(transfer_funds.get_target_accounts(), trg_acc_no)
+        log.info("Clicking the Transfer button.")
+        transfer_funds.get_transfer_button().click()
+        with allure.step("Step 1: Verify the debit on source account"):
+            log.info("Clicking the Accounts Overview link.")
+            accounts_overview = login_logout[0].get_accounts_overview_page()
+            log.info("Clicking the source account link.")
+            activity_page = accounts_overview.select_account_number(src_acc_no)
+            log.info("Selecting the Debit from the Type list.")
+            activity_page.select_value_from_dropdown_text(activity_page.get_activity_type(), "Debit")
+            log.info("Clicking the Go button.")
+            activity_page.get_activity_button().click()
+            transaction = activity_page.get_transaction_by_index(-1)
+            log.info(f"Verifying if the transaction name is {activity_page.FUNDS_SENT}")
+            assert transaction[1] == activity_page.FUNDS_SENT
+            log.info(f"Verifying if the transaction amount is {amount}.")
+            assert str(transaction[2]).split(".")[0] == amount
+        with allure.step("Step 2: Verify the credit on target account"):
+            log.info("Clicking the Accounts Overview link.")
+            accounts_overview = login_logout[0].get_accounts_overview_page()
+            log.info("Clicking the target account link.")
+            activity_page = accounts_overview.select_account_number(trg_acc_no)
+            log.info("Selecting the Credit from the Type list.")
+            activity_page.select_value_from_dropdown_text(activity_page.get_activity_type(), "Credit")
+            log.info("Clicking the Go button.")
+            activity_page.get_activity_button().click()
+            transaction = activity_page.get_transaction_by_index(-1)
+            log.info(f"Verifying if the transaction name is {activity_page.FUNDS_RECEIVED}")
+            assert transaction[1] == activity_page.FUNDS_RECEIVED
+            log.info(f"Verifying if the transaction amount is {amount}.")
+            assert str(transaction[3]).split(".")[0] == amount
 
