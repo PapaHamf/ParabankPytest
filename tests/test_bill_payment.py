8705ef76136997b8c8f7112ffba7cddd1eb15b15
PapaHamf <jaroslaw.puchala@gmail.com>
2025-05-02 22:10:04 +0200

    Allure steps, assert order 02.05

--- tests/test_bill_payment.py
+++ tests/test_bill_payment.py
@@ -9,7 +9,6 @@ from utils.myfaker import MyFaker
 from pageobjects.homepage import HomePage
 from pageobjects.basepage import BasePage
 
-
 class TestOpenAccount(BaseClass):
 
     @allure.title("Logging in/out the user")
@@ -52,7 +51,7 @@ class TestOpenAccount(BaseClass):
     @allure.description("This test attempts to pay the bill for the utility company.")
     @pytest.mark.smoke
     @pytest.mark.usefixtures("login_logout")
-    def test_bill_payment_postive(self, login_logout):
+    def test_bill_payment_positive(self, login_logout):
         """
         Tests the bill payment for the utility company.
         :return:
@@ -60,55 +59,57 @@ class TestOpenAccount(BaseClass):
         log = self.get_logger()
         log.info("Clicking the Bill Pay link.")
         bill_pay = login_logout[0].get_bill_pay_page()
-        # Getting the faker-generated data
-        payment_data = MyFaker.bill_data_all_fields(company = "PGNiG")[0]
-        data_collection = ExcelData("Bill Payment", "New payment")
-        company_name = payment_data["payee"]
-        log.info(f"Entering the payee name: {company_name}")
-        data_collection.add_data("payee", company_name)
-        bill_pay.get_payee_name().send_keys(company_name)
-        address = payment_data["payeeaddress"]
-        log.info(f"Entering the payee address: {address}")
-        data_collection.add_data("payeeaddress", address)
-        bill_pay.get_payee_street().send_keys(address)
-        city = payment_data["city"]
-        log.info(f"Entering the payee city: {city}")
-        data_collection.add_data("city", city)
-        bill_pay.get_payee_city().send_keys(city)
-        state = payment_data["state"]
-        log.info(f"Entering the state: {state}")
-        data_collection.add_data("state", state)
-        bill_pay.get_payee_state().send_keys(state)
-        post_code = payment_data["postcode"]
-        log.info(f"Entering the post code: {post_code}")
-        data_collection.add_data("postcode", post_code)
-        bill_pay.get_payee_post_code().send_keys(post_code)
-        phone_number = payment_data["phonenumber"]
-        log.info(f"Entering the phone number: {phone_number}")
-        data_collection.add_data("phonenumber", phone_number)
-        bill_pay.get_payee_phonenumber().send_keys(phone_number)
-        account_number = payment_data["account"]
-        log.info(f"Entering the account number: {account_number}")
-        data_collection.add_data("account", account_number)
-        bill_pay.get_payee_account_number().send_keys(account_number)
-        verify_account = payment_data["verifyaccount"]
-        log.info(f"Entering the verify account number: {verify_account}")
-        data_collection.add_data("verifyaccount", verify_account)
-        bill_pay.get_payee_verify_account().send_keys(verify_account)
-        amount = payment_data["amount"]
-        log.info(f"Entering the amount: {amount}")
-        data_collection.add_data("amount", amount)
-        bill_pay.get_payment_amount().send_keys(amount)
-        # Select random account from the available list
-        account_list = bill_pay.get_list_values(bill_pay.get_source_account())
-        src_acc_no = random.choice(account_list)
-        log.info(f"Selecting the account number: {src_acc_no}")
-        data_collection.add_data("sourceaccount", src_acc_no)
-        bill_pay.select_value_from_dropdown_text(bill_pay.get_source_account(), src_acc_no)
-        log.info("Clicking the payment button.")
-        bill_pay.get_payment_button().click()
-        data_collection.save_data()
-        assert bill_pay.get_payment_success_title().text == bill_pay.BILL_PAYMENT_SUCCESS_MSG
+        with allure.step("Step 1: Complete the bill payment"):
+            # Getting the faker-generated data
+            payment_data = MyFaker.bill_data_all_fields(company = "PGNiG")[0]
+            data_collection = ExcelData("Bill Payment", "New payment")
+            company_name = payment_data["payee"]
+            log.info(f"Entering the payee name: {company_name}")
+            data_collection.add_data("payee", company_name)
+            bill_pay.get_payee_name().send_keys(company_name)
+            address = payment_data["payeeaddress"]
+            log.info(f"Entering the payee address: {address}")
+            data_collection.add_data("payeeaddress", address)
+            bill_pay.get_payee_street().send_keys(address)
+            city = payment_data["city"]
+            log.info(f"Entering the payee city: {city}")
+            data_collection.add_data("city", city)
+            bill_pay.get_payee_city().send_keys(city)
+            state = payment_data["state"]
+            log.info(f"Entering the state: {state}")
+            data_collection.add_data("state", state)
+            bill_pay.get_payee_state().send_keys(state)
+            post_code = payment_data["postcode"]
+            log.info(f"Entering the post code: {post_code}")
+            data_collection.add_data("postcode", post_code)
+            bill_pay.get_payee_post_code().send_keys(post_code)
+            phone_number = payment_data["phonenumber"]
+            log.info(f"Entering the phone number: {phone_number}")
+            data_collection.add_data("phonenumber", phone_number)
+            bill_pay.get_payee_phonenumber().send_keys(phone_number)
+            account_number = payment_data["account"]
+            log.info(f"Entering the account number: {account_number}")
+            data_collection.add_data("account", account_number)
+            bill_pay.get_payee_account_number().send_keys(account_number)
+            verify_account = payment_data["verifyaccount"]
+            log.info(f"Entering the verify account number: {verify_account}")
+            data_collection.add_data("verifyaccount", verify_account)
+            bill_pay.get_payee_verify_account().send_keys(verify_account)
+            amount = payment_data["amount"]
+            log.info(f"Entering the amount: {amount}")
+            data_collection.add_data("amount", amount)
+            bill_pay.get_payment_amount().send_keys(amount)
+            # Select random account from the available list
+            account_list = bill_pay.get_list_values(bill_pay.get_source_account())
+            src_acc_no = random.choice(account_list)
+            log.info(f"Selecting the account number: {src_acc_no}")
+            data_collection.add_data("sourceaccount", src_acc_no)
+            bill_pay.select_value_from_dropdown_text(bill_pay.get_source_account(), src_acc_no)
+            log.info("Clicking the payment button.")
+            bill_pay.get_payment_button().click()
+            data_collection.save_data()
+        with allure.step("Step 2: Verify the success message"):
+            assert bill_pay.get_payment_success_title().text == bill_pay.BILL_PAYMENT_SUCCESS_MSG
 
     @allure.parent_suite("Tests for Parabank application")
     @allure.suite("Tests for bill payment")
@@ -128,55 +129,57 @@ class TestOpenAccount(BaseClass):
         log = self.get_logger()
         log.info("Clicking the Bill Pay link.")
         bill_pay = login_logout[0].get_bill_pay_page()
-        # Getting the faker-generated data
-        payment_data = MyFaker.bill_data_all_fields()[0]
-        data_collection = ExcelData("Bill Payment", "New payment")
-        company_name = payment_data["payee"]
-        log.info(f"Entering the payee name: {company_name}")
-        data_collection.add_data("payee", company_name)
-        bill_pay.get_payee_name().send_keys(company_name)
-        address = payment_data["payeeaddress"]
-        log.info(f"Entering the payee address: {address}")
-        data_collection.add_data("payeeaddress", address)
-        bill_pay.get_payee_street().send_keys(address)
-        city = payment_data["city"]
-        log.info(f"Entering the payee city: {city}")
-        data_collection.add_data("city", city)
-        bill_pay.get_payee_city().send_keys(city)
-        state = payment_data["state"]
-        log.info(f"Entering the state: {state}")
-        data_collection.add_data("state", state)
-        bill_pay.get_payee_state().send_keys(state)
-        post_code = payment_data["postcode"]
-        log.info(f"Entering the post code: {post_code}")
-        data_collection.add_data("postcode", post_code)
-        bill_pay.get_payee_post_code().send_keys(post_code)
-        phone_number = payment_data["phonenumber"]
-        log.info(f"Entering the phone number: {phone_number}")
-        data_collection.add_data("phonenumber", phone_number)
-        bill_pay.get_payee_phonenumber().send_keys(phone_number)
-        account_number = payment_data["account"]
-        log.info(f"Entering the account number: {account_number}")
-        data_collection.add_data("account", account_number)
-        bill_pay.get_payee_account_number().send_keys(account_number)
-        verify_account = payment_data["verifyaccount"]
-        log.info(f"Entering the verify account number: {verify_account}")
-        data_collection.add_data("verifyaccount", verify_account)
-        bill_pay.get_payee_verify_account().send_keys(verify_account)
-        amount = "-0.01"
-        log.info(f"Entering the amount: {amount}")
-        data_collection.add_data("amount", amount)
-        bill_pay.get_payment_amount().send_keys(amount)
-        # Select random account from the available list
-        account_list = bill_pay.get_list_values(bill_pay.get_source_account())
-        src_acc_no = random.choice(account_list)
-        log.info(f"Selecting the account number: {src_acc_no}")
-        data_collection.add_data("sourceaccount", src_acc_no)
-        data_collection.save_data()
-        bill_pay.select_value_from_dropdown_text(bill_pay.get_source_account(), src_acc_no)
-        log.info("Clicking the payment button.")
-        bill_pay.get_payment_button().click()
-        assert bill_pay.get_payee_amount_invalid_error().text == bill_pay.BILL_PAYMENT_INVALID_AMOUNT
+        with allure.step("Step 1: Complete the bill payment"):
+            # Getting the faker-generated data
+            payment_data = MyFaker.bill_data_all_fields()[0]
+            data_collection = ExcelData("Bill Payment", "New payment")
+            company_name = payment_data["payee"]
+            log.info(f"Entering the payee name: {company_name}")
+            data_collection.add_data("payee", company_name)
+            bill_pay.get_payee_name().send_keys(company_name)
+            address = payment_data["payeeaddress"]
+            log.info(f"Entering the payee address: {address}")
+            data_collection.add_data("payeeaddress", address)
+            bill_pay.get_payee_street().send_keys(address)
+            city = payment_data["city"]
+            log.info(f"Entering the payee city: {city}")
+            data_collection.add_data("city", city)
+            bill_pay.get_payee_city().send_keys(city)
+            state = payment_data["state"]
+            log.info(f"Entering the state: {state}")
+            data_collection.add_data("state", state)
+            bill_pay.get_payee_state().send_keys(state)
+            post_code = payment_data["postcode"]
+            log.info(f"Entering the post code: {post_code}")
+            data_collection.add_data("postcode", post_code)
+            bill_pay.get_payee_post_code().send_keys(post_code)
+            phone_number = payment_data["phonenumber"]
+            log.info(f"Entering the phone number: {phone_number}")
+            data_collection.add_data("phonenumber", phone_number)
+            bill_pay.get_payee_phonenumber().send_keys(phone_number)
+            account_number = payment_data["account"]
+            log.info(f"Entering the account number: {account_number}")
+            data_collection.add_data("account", account_number)
+            bill_pay.get_payee_account_number().send_keys(account_number)
+            verify_account = payment_data["verifyaccount"]
+            log.info(f"Entering the verify account number: {verify_account}")
+            data_collection.add_data("verifyaccount", verify_account)
+            bill_pay.get_payee_verify_account().send_keys(verify_account)
+            amount = "-0.01"
+            log.info(f"Entering the amount: {amount}")
+            data_collection.add_data("amount", amount)
+            bill_pay.get_payment_amount().send_keys(amount)
+            # Select random account from the available list
+            account_list = bill_pay.get_list_values(bill_pay.get_source_account())
+            src_acc_no = random.choice(account_list)
+            log.info(f"Selecting the account number: {src_acc_no}")
+            data_collection.add_data("sourceaccount", src_acc_no)
+            data_collection.save_data()
+            bill_pay.select_value_from_dropdown_text(bill_pay.get_source_account(), src_acc_no)
+            log.info("Clicking the payment button.")
+            bill_pay.get_payment_button().click()
+        with allure.step("Step 2: Verify the error message"):
+            assert bill_pay.get_payee_amount_invalid_error().text == bill_pay.BILL_PAYMENT_INVALID_AMOUNT
 
     @allure.parent_suite("Tests for Parabank application")
     @allure.suite("Tests for bill payment")
