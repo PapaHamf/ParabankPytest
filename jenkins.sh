fee12a395ee58ed67fd0c4fd0aaf27986437a747
PapaHamf <jaroslaw.puchala@gmail.com>
2025-06-15 00:42:19 +0200

    Increased script wait time 15.06

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,7 @@ echo "Installing the parabank docker application"
 docker pull parasoft/parabank
 echo "Starting the parabank docker application"
 docker run -d -p 8080:8080 -p 61616:61616 -p 9001:9001 parasoft/parabank
-sleep 20
+echo "Waiting for the service startup (3 min)"
+sleep 3m
 echo "Starting the tests"
 python3 -m pytest
--- start_tests.sh
+++ start_tests.sh
@@ -2,8 +2,8 @@
 
 echo "Starting the parabank docker application"
 docker run -d -p 8080:8080 -p 61616:61616 -p 9001:9001 parasoft/parabank
-echo "Waiting for the service startup"
-sleep 20
+echo "Waiting for the service startup (3 min)"
+sleep 3m
 echo "Starting the tests"
 python3 -m pytest
 echo "Generating the report"
