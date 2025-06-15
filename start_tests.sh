370529e2ff785aea02a5cc03c9dcf1c922742884
PapaHamf <jaroslaw.puchala@gmail.com>
2025-06-15 17:05:52 +0200

    Script fixes 15.06

--- start_tests.sh
+++ start_tests.sh
@@ -4,8 +4,13 @@ echo "Installing the python packages"
 python3 -m pip install --break-system-packages -r requirements.txt
 echo "Starting the parabank docker application"
 docker run -d -p 8080:8080 -p 61616:61616 -p 9001:9001 parasoft/parabank
-echo "Waiting for the service startup (3 min)"
-sleep 3m
+# Temporary workaround, sometimes the HYSQL wont start up no matter what..
+echo "Waiting for the service startup (30 s)"
+sleep 30
+curl http://localhost:8080/parabank/index.htm
+sleep 15
+curl http://localhost:8080/parabank/admin.htm
+sleep 15
 echo "Starting the tests"
 python3 -m pytest
 echo "Generating the report"
